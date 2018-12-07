91cda83410f3ef4fc6f8432666c6191a62053b3b
adrian <adrian.misko5@gmail.com>
2018-12-07 15:13:22 +0100

    add event handler

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    private JFXButton button;
+
+    public Controller() {}
+
+    @FXML
+    private void buttonClick() {
+        //get access to ksession?
+    }
+
 }
