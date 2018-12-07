970e1c2ffd495e1a787b50d1bd5e9078278ca5e7
adrian <adrian.misko5@gmail.com>
2018-12-07 15:01:40 +0100

    continue cleaning upi

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 import java.io.IOException;
 
@@ -16,12 +22,21 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
     }
 
 
 
-
     public static void main(String[] args) {
+
+        KieServices ks = KieServices.Factory.get();
+        BasicConfigurator.configure();
+        Logger.getLogger(Main.class).setLevel(Level.OFF);
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession session = kContainer.newKieSession("ksession-rules");
+
+        session.fireAllRules();
+
         launch(args);
 
     }
